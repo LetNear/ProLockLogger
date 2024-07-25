9ebe746eaefa1672e166fd869f0e1de3525e9e6c
LetNear <bopenales@my.cspc.edu.ph>
2024-07-25 14:21:52 +0800

    [Backend Fix] fix the issue in audits where it still shows the password in audits

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -34,7 +34,8 @@ class UserResource extends Resource
                 Forms\Components\DateTimePicker::make('email_verified_at'),
                 Forms\Components\TextInput::make('password')
                     ->password()
-                    
+                    ->dehydrated(fn ($state) => filled($state))
+                    ->required(fn (string $context): bool => $context === 'create')
                     ->maxLength(255),
             ]);
     }
