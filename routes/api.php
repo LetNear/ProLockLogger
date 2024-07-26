02cf74fc805845885c0ef962d24274ab938242d6
LetNear <bopenales@my.cspc.edu.ph>
2024-07-26 21:28:45 +0800

    [API] Added api testing

---
+++ routes/api.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Http\Controllers\NfcTagController;
+use App\Http\Controllers\UserController;
+
+Route::apiResource('posts', UserController::class);
+Route::apiResource('nfc-tags', NfcTagController::class);
