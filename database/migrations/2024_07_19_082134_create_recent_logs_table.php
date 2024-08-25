95de863ad28801eaa8a9abf6347791e36f9c4f66
LetNear <bopenales@my.cspc.edu.ph>
2024-08-26 01:26:30 +0800

    Update 2024_07_19_082134_create_recent_logs_table.php

--- database/migrations/2024_07_19_082134_create_recent_logs_table.php
+++ database/migrations/2024_07_19_082134_create_recent_logs_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->string('year')->nullable();
             $table->string('time_in')->nullable();
             $table->string('time_out')->nullable();
+            $table->string('rfid_number')->nullable();
       
             $table->timestamps();
         });
