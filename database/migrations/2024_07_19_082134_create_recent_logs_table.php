263d476aa943a8ade0864f8e10aa8619668e6271
LetNear <bopenales@my.cspc.edu.ph>
2024-08-28 01:12:23 +0800

    Update 2024_07_19_082134_create_recent_logs_table.php

--- database/migrations/2024_07_19_082134_create_recent_logs_table.php
+++ database/migrations/2024_07_19_082134_create_recent_logs_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->id();
             $table->foreignIdFor(Block::class, 'block_id')->nullable();
             $table->foreignIdFor(Nfc::class, 'id_card_id')->nullable();
-            $table->foreignIdFor(UserInformation::class, 'user_number')->nullable();
+            $table->string('user_number')->nullable();
             $table->string('year')->nullable();
             $table->string('time_in')->nullable();
             $table->string('time_out')->nullable();
