76e1e038accf4f72cbcdee66f0936f59454b785a
LetNear <bopenales@my.cspc.edu.ph>
2024-09-02 22:38:47 +0800

    Create LabAttendancePolicy.php

---
+++ app/Policies/LabAttendancePolicy.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\LabAttendance;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class LabAttendancePolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->hasRole('Administrator');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, LabAttendance $labAttendance): bool
+    {
+        return $user->hasRole('Administrator');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->hasRole('Administrator');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, LabAttendance $labAttendance): bool
+    {
+        return $user->hasRole('Administrator');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, LabAttendance $labAttendance): bool
+    {
+        return $user->hasRole('Administrator');
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, LabAttendance $labAttendance): bool
+    {
+        return $user->hasRole('Administrator');
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, LabAttendance $labAttendance): bool
+    {
+        return $user->hasRole('Administrator');
+    }
+}
