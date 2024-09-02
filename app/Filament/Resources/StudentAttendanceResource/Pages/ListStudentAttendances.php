b7169eb3da7f990b0d90fc6dd787003d33b597ed
LetNear <bopenales@my.cspc.edu.ph>
2024-09-03 02:12:43 +0800

    fix

--- app/Filament/Resources/StudentAttendanceResource/Pages/ListStudentAttendances.php
+++ app/Filament/Resources/StudentAttendanceResource/Pages/ListStudentAttendances.php
@@ -13,7 +13,7 @@ class ListStudentAttendances extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            // Actions\CreateAction::make(),
         ];
     }
 }
