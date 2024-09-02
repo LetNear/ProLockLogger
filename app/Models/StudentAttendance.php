2c1bbe7a0353984c63ef408a174385aa3dce3481
LetNear <bopenales@my.cspc.edu.ph>
2024-09-02 23:02:08 +0800

    Update StudentAttendance.php

--- app/Models/StudentAttendance.php
+++ app/Models/StudentAttendance.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class StudentAttendance extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'course',
+        'year',
+        'block',
+        'student_number',
+        'time_in',
+        'time_out',
+        'status',
+    ];
 }
