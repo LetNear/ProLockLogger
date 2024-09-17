a57cc2686c644a5715106a09cd324abe8ef34711
LetNear <bopenales@my.cspc.edu.ph>
2024-09-17 12:52:12 +0800

    Update RecentLogsController.php

--- app/Http/Controllers/RecentLogsController.php
+++ app/Http/Controllers/RecentLogsController.php
@@ -48,7 +48,7 @@ class RecentLogsController extends Controller
                 ->get()
                 ->map(function ($log) {
                     return [
-                        'user_name' => $log->user_number ?? ($log->userInformation->user->name ?? 'Unknown'),
+                        'user_name' => $log->userInformation->user->name ?? 'Unknown',
                         'block_name' => $log->block->block ?? 'Unknown',
                         'year' => $log->year ?? 'Unknown',
                         'time_in' => $log->time_in ?? 'N/A',
